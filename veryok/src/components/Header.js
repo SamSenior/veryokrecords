66e23640bbffcf22306b20bb29660d10b9b2092e
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-18 13:33:29 +1200

    reinstalled next

--- veryok/src/components/Header.js
+++ veryok/src/components/Header.js
@@ -1,18 +1,18 @@
 "use client";
 
 import React from "react";
-// import { LogoutButton } from "./LogoutButton";
-// import { useSession } from "next-auth/react";
+import { LogoutButton } from "./LogoutButton";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
-  // const { data: session } = useSession({
-  //   required: false,
-  // });
+  const { data: session } = useSession({
+    required: false,
+  });
 
   return (
     <header className="sticky top-0 w-full h-12 bg-black text-white py-7 px-7 flex items-center justify-between">
-      <img className="max-h-8" src="/images/veryok1.png" alt="Logo" />
-      {/* {session && <LogoutButton />} */}
+      <img className="max-h-8" src="/images/logo-transparent.png" alt="Logo" />
+      {session && <LogoutButton />}
     </header>
   );
 };
