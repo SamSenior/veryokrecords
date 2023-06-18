3f070af918e50d3ac4f935a33404382ec668fbbf
Sam Senior <samuelsenior@developersinstitute.co.nz>
2023-06-18 13:04:50 +1200

    added logo and header component

---
+++ veryok/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-black text-white p-4">
+      <div className="flex items-center">
+        <span className="mr-4">©2023 by Developers Institute</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
